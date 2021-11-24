c5d9ee778e1cf7e2fe27784da7a1da816039fac8
khf116 <khf116@khf.ru>
2021-11-24 22:19:10 +1000

    add CounterButtomComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,9 +4,29 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+
+  methods: {
+    setCount(value) {
+      this.$emit('update:count', ++value);
+    },
+  },
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
 
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `
+    <button 
+      type="button"
+      @click="setCount(this.count)"
+    >
+      {{ count }}
+    </button>
+  `,
 });
