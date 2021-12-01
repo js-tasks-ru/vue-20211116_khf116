0d3b6c69ab1062760844f731908c35b4764c7577
khf116 <khf116@khf.ru>
2021-12-01 12:08:12 +1000

    add theToaster

---
+++ 04-vue-cli/02-TheToaster/components/UiToast.vue
@@ -0,0 +1,110 @@
+<template>
+  <!-- <div :class="toastClass" v-if="isVisible"> -->
+  <div class="toast" :class="toastClass">
+    <ui-icon class="toast__icon" :icon="toastIcon" />
+    <span>{{ item.message }}</span>
+  </div>
+</template>
+
+<script>
+import UiIcon from './UiIcon';
+
+const toastIcons = {
+  success: 'check-circle',
+  error: 'alert-circle',
+};
+
+const toastClasses = {
+  success: 'toast toast_success',
+  error: 'toast toast_error',
+};
+
+
+export default {
+  name: 'UiToast',
+
+  components: { UiIcon },
+
+  // data () {
+  //   return {
+  //     isVisible: true,
+  //     timerId: null,
+  //   }
+  // },
+
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  
+  // mounted() {
+  //   this.startHiding();
+  // },
+
+  // methods: {    
+  //   startHiding() {
+  //     this.timerId = setTimeout(() => this.isVisible = false, this.delayValue);
+  //   },
+
+  //   hide() {
+  //     this.isVisible = false;
+  //     if (this.timerId)
+  //       clearTimeout(this.timerId);
+  //   }
+  // },
+
+
+  computed: {
+
+    toastIcon() {
+      return toastIcons[this.item.type];
+    },
+
+    toastClass() {
+      return toastClasses[this.item.type];
+    },
+
+    // delayValue() {
+    //   return this.item.delay || 5000;
+    // },
+
+  },
+
+};
+</script>
+
+<style scoped>
+
+
+.toast {
+  display: flex;
+  flex: 0 0 auto;
+  flex-direction: row;
+  align-items: center;
+  padding: 16px;
+  background: #ffffff;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+  font-size: 18px;
+  line-height: 28px;
+  width: auto;
+}
+
+.toast + .toast {
+  margin-top: 20px;
+}
+
+.toast__icon {
+  margin-right: 12px;
+}
+
+.toast.toast_success {
+  color: var(--green);
+}
+
+.toast.toast_error {
+  color: var(--red);
+}
+</style>
---
+++ 04-vue-cli/02-TheToaster/components/UiToastList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="toasts">
+    <span v-for="toast in toasts" :key="toast.id">
+      <ui-toast :item="toast" v-if="toast.visible" />
+    </span>    
+  </div>
+</template>
+
+<script>
+import UiToast from './UiToast';
+
+export default {
+  name: 'UiToastList',
+
+  props: {
+    toasts: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  components: { UiToast },
+
+
+};
+</script>
+
+<style scoped>
+.toasts {
+  position: fixed;
+  bottom: 8px;
+  right: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  white-space: pre-wrap;
+  z-index: 999;
+}
+</style>
